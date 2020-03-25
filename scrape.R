f9a9013d33b867739b4d18b8593b762ffacb235a
Andrew Dyck <andrewjdyck@users.noreply.github.com>
2020-03-25 07:39:06 -0600

    Create scrape.R

---
+++ scrape.R
@@ -0,0 +1,33 @@
+library(rvest)
+library(jsonlite)
+
+dt <- '20200325'
+url <- 'https://www.saskatchewan.ca/government/health-care-administration-and-provider-resources/treatment-procedures-and-guidelines/emerging-public-health-issues/2019-novel-coronavirus/cases-and-risk-of-covid-19-in-saskatchewan'
+
+
+
+dd <- read_html(url) %>%
+  html_nodes('.compacttable') %>%
+  html_table()
+
+tt <- dd[[1]][2:nrow(dd[[1]]), ]
+names(tt) <- dd[[1]][1, ]
+
+write.csv(tt, paste0('./data/covid_sk_', dt, '.csv'), row.names=FALSE)
+
+# Read summary
+summ <- read_html(url) %>%
+  html_nodes('.general-content')
+
+
+lst <- html_node(summ, 'ul')
+ll <- lst[[1]]
+
+l1 <- ll %>%
+  html_text() %>% 
+  strsplit(split = "\n") %>%
+  unlist()
+
+write(toJSON(l1), paste0('./data/lst_', dt, '.json'))
+
+readdate <- html_nodes(summ, 'p strong')
