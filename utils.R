7f15a3d12854f6bc08e396606a5069de15ebdabb
Andy Dyck <andrew@andrewdyck.com>
2020-03-31 21:18:05 +0000

    detection rate from github

--- utils.R
+++ utils.R
@@ -10,6 +10,26 @@ estDetRate <- function(confirmedCases, hospitalizations, tts=5, hospitalizationR
   return(detectionRate)
 }
 
+# estimates detection rate based on assumptions about cfr, ttd
+detRate<-function(infd, deaths, cfr = 0.01, ttd=17, window=5){
+  obs<-c(rep(NA, window), diff(infd, window)) # observed new cases
+  deathDiff<-diff(deaths, window) # observed new deaths
+  expd<-deathDiff/cfr #expected new cases given cfr
+  expd<-expd[-(1:(ttd-window))]
+  expd<-c(expd, rep(NA, ttd))
+  detRate<-obs/expd
+  detRate[detRate==0]<-NA
+  detRate[is.infinite(detRate)]<-NA
+  out<-mean(detRate, na.rm = TRUE)
+  if (is.nan(out)) return(NA)
+  if (out>1) out<-1
+  out
+}
+
+growthRate <- function(cases) {
+  return(c(NA, diff(cases)/cases[1:(length(cases)-1)]))
+}
+
 # Simple projection based on growth over last inWindow days
 # returns coefficients
 projSimpleSlope<-function(rawN, rawTime, inWindow=10){
