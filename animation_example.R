32e54af0b15e5559f7327a579dcd1f4d0cd36ee8
andrewjdyck <andrew@andrewdyck.com>
2020-04-12 07:45:07 -0600

    forecast animation cleanup

--- animation_example.R
+++ animation_example.R
@@ -7,10 +7,14 @@ library(reshape2)
 library(gganimate)
 indata <- read_csv('./data/cases-sk.csv')
 indata$Active <- indata$Cases-indata$Recovered-indata$Deaths
+
+# Data melted so that it can be used by ggplot2
 ggdata <- indata %>%
   select(Date, Recovered, Active, Deaths) %>%
   melt(id="Date")
   
+
+# Animated bell curve of cumulative cases and recoveries
 anim <- ggplot(ggdata, aes(x=Date, y=value, fill=variable)) + 
   geom_area() +
   scale_fill_manual(values=c("green", "orange", "red")) + 
@@ -21,84 +25,6 @@ anim_out <- animate(anim, end_pause=10)
 anim_save('./output/cases_gif.gif', anim_out)
 
 
-dd <- data.frame(time=c(1:10, 1:10), day=c(rep(1, 10), rep(2, 10)))
-dd$value <- dd$day*dd$time + 10
-
-pp <- ggplot(dd, aes(x=time, y=value, group=day)) + 
-  geom_line()
-
-pp
-
-pp + 
-  transition_states(day, transition_length = 2, state_length = 1)
 
 
-
-library(gapminder)
-library(ggplot2)
-library(gganimate)
-p <- ggplot(gapminder, aes(x = gdpPercap, y=lifeExp, size = pop, colour = country)) +
-  geom_point(show.legend = FALSE, alpha = 0.7) +
-  scale_color_viridis_d() +
-  scale_size(range = c(2, 12)) +
-  scale_x_log10() +
-  labs(x = "GDP per capita", y = "Life expectancy")
-
-p + transition_time(year) +
-  labs(title = "Year: {frame_time}")
-
-
-
-library(ggplot2)
-library(forecast)
-wine.fit <- hw(wineind,h=48)
-plot(wine.fit)
-autoplot(wine.fit)
-
-
-aa <- autoplot(wine.fit) +
-  geom_forecast()
-aa +
-  transition_time(Point)
-
-
-
-
-
-
-
-library(ggplot2)
-tt <- autoplot(USAccDeaths) + geom_forecast()
-
-tt +
-  transition_reveal()
-
-lungDeaths <- cbind(mdeaths, fdeaths)
-autoplot(lungDeaths) + geom_forecast()
-
-# Using fortify.ts
-p <- ggplot(aes(x=x, y=y), data=USAccDeaths)
-p <- p + geom_line()
-p + geom_forecast()
-
-# Without fortify.ts
-data <- data.frame(USAccDeaths=as.numeric(USAccDeaths), time=as.numeric(time(USAccDeaths)))
-p <- ggplot(aes(x=time, y=USAccDeaths), data=data)
-p <- p + geom_line()
-p + geom_forecast()
-
-p + geom_forecast(h=60)
-p <- ggplot(aes(x=time, y=USAccDeaths), data=data)
-p + geom_forecast(level=c(70,98))
-p + geom_forecast(level=c(70,98),colour="lightblue")
-
-#Add forecasts to multivariate series with colour groups
-lungDeaths <- cbind(mdeaths, fdeaths)
-autoplot(lungDeaths) + geom_forecast(forecast(mdeaths), series="mdeaths")
-
-
-
-fcast <- splinef(airmiles,h=5)
-plot(fcast)
-autoplot(fcast)
 
--- forecast.R
+++ forecast.R
@@ -6,26 +6,6 @@ library(plyr)
 source('./utils.R')
 
 indata <- readr::read_csv('./data/cases-sk.csv')
-# cases <- ts(indata$Cases, order.by=indata$Date)
-cases <- indata$Cases
-
-
-tenDayProj <- function(cases, nn) {
-  projectedCases <- floor(as.data.frame(projSimple(cases, 1:length(cases), inWindow = 10, outWindow = 10)$y)$fit)
-  fcastCases <- projectedCases[length(projectedCases)]
-  return(fcastCases)
-}
-
-forecast <- c(
-  rep(NA, 18), 
-  sapply(9:length(cases), function(x) tenDayProj(cases[1:x]))
-)
-
-dd <- data.frame(
-  cases = c(cases, rep(NA, 10)),
-  forecast = forecast
-)
-
 
 
 # forecasted series over time
@@ -35,36 +15,27 @@ projDf <- function(df, endDay) {
   casesSubset <- df$Cases[1:endDay]
   fcastdate <- df$Date[nn]
   newdates <- c(indata$Date, as.Date(1:10 + as.numeric(df$Date[nrow(df)]), origin = '1970-01-01'))
-  pc <- floor(as.data.frame(projSimple(casesSubset, 1:nn, inWindow = 10, outWindow = (length(cases)+10-endDay))$y))
+  pc <- floor(as.data.frame(projSimple(casesSubset, 1:nn, inWindow = 10, outWindow = (nrow(df)+10-endDay))$y))
   pc[1:nn, c('fit', 'lwr', 'upr')] <- cbind(casesSubset, casesSubset, casesSubset)
   pc$fcastday <- df$Date[nn]
   pc$Date <- newdates
   return(pc)
 }
 
-projAll <- function(df) {
-  lapply(10:nrow(df), function(x) projDf(df, x))
-}
-
-tt <- projAll(indata)
-
-# x <- 19
-# ggplot(tt[x][[1]], aes(x = day, y = fit)) + 
-#   # geom_line(colour='blue') +
-#   geom_smooth(aes(x=day, y=fit, ymax=upr, ymin=lwr), 
-#               colour='red', data=tt[x][[1]], stat='identity')
 
 
-# dd <- rbind(tt[[10]], tt[[20]])
-dd <- ldply(tt)
-pa <- ggplot(dd, aes(x = Date, y = fit, group=fcastday)) + 
-  # geom_line(colour='blue') +
+# Prepare forecasted data for ggplot2 and save
+dd <- ldply(lapply(10:nrow(indata), function(x) projDf(indata, x)))
+write.csv(dd, paste0('./data/simple_forecasts.csv'), row.names=FALSE)
+
+forecastedCases <- read_csv('./data/simple_forecasts.csv')
+fanim <- ggplot(forecastedCases, aes(x = Date, y = fit, group=fcastday)) + 
   geom_smooth(aes(group=fcastday, ymax=upr, ymin=lwr), 
                stat='identity') 
 
-pa
-pa_out <- animate(pa + 
+
+fanim_out <- animate(fanim + 
   transition_states(fcastday) +
-  labs(title = 'Forecast day: {previous_state}', y='COVID-19 Cases', x='Date') + 
+  labs(title = 'COVID-19 forecasted cases: {previous_state}', y='COVID-19 Cases', x='Date') + 
   view_follow(), end_pause = 30)
-anim_save('./output/forecast.gif', pa_out)
+anim_save('./output/forecast.gif', fanim_out)
