3ed854abc24560b2ed2df9c2e0ad1d2eab07b52b
Andy Dyck <andrew@andrewdyck.com>
2020-05-03 14:40:12 -0600

    automating run of dashboard

--- run.R
+++ run.R
@@ -1,7 +1,9 @@
 #!/usr/bin/Rscript
 
+# source('/home/andy/covid19-sask/scrape.R')
+
 Sys.setenv(RSTUDIO_PANDOC="/usr/lib/rstudio-server/bin/pandoc")
 
-rmarkdown::render('/home/andy/covid19-sask/dash.Rmd', output_dir='/home/andy/covid19-sask')
+rmarkdown::render('/home/andy/covid19-sask/dashboard/dash.Rmd', output_dir='/home/andy/covid19-sask/dashboard')
 
-file.copy('/home/andy/covid19-sask/dash.html', '/srv/shiny-server/covid19/index.html', overwrite=TRUE)
+file.copy('/home/andy/covid19-sask/dashboard/dash.html', '/srv/shiny-server/covid19/index.html', overwrite=TRUE)
